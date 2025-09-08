039af0890a428e6e8095e521d47c1259b45c5702
victor-kofa <victor.owusu@kofa.co>
2025-09-08 00:08:34 +0000

    types

---
+++ types/index.ts
@@ -0,0 +1,20 @@
+export interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;    
+}
+
+export interface Comment {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+}
